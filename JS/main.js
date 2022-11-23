2c6a014367cbdc58b080a81acb3b0dccc0b43f7a
MarcoAgosta <agostamarco@libero.it>
2022-11-23 16:51:24 +0100

    Completata la prima milestone

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+const{createApp} = Vue;
+createApp({
+    data(){
+        return {
+            todolist:[
+                {
+                    text: "Dare da mangiare al cane",
+                    done: false,
+                },{
+                    text: "Andare a fare la spesa",
+                    done: false,
+                }
+            ],
+        }
+    },
+    methods:{
+
+        changeDone(i){
+            
+            if (this.todolist[i].done==false){
+                this.todolist[i].done=true;
+
+            } else if (this.todolist[i].done==true){
+                this.todolist[i].done=false;
+            }
+        },
+    },
+
+}).mount("#app")
